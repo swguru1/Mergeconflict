c27caa5676d53d6f93ce0e05485209d5b5434b40
swguru <swguru@DIN16002798.corp.capgemini.com>
2016-11-16 14:28:58 +0530

    branch2 commit

--- src/Test.java
+++ src/Test.java
@@ -3,8 +3,8 @@ public class Test
 {
 	public static void main(String[] args) {
 		System.out.println("master");
-		System.out.println("Branch2");
-		System.out.println("branch1");
+		System.out.println("Branch2 commit");
+		
 	}
 	
 }
