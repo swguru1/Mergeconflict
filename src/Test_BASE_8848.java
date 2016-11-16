b77d4bcb498f9c8e378752faf8af06a515bc28f0
swguru <swguru@DIN16002798.corp.capgemini.com>
2016-11-16 16:42:27 +0530

    sdda

--- src/Test_BASE_8848.java
+++
@@ -1,9 +0,0 @@
-
-public class Test 
-{
-	public static void main(String[] args) {
-		System.out.println("master");
-		System.out.println("branch1 commit");
-	}
-	
-}
--- src/Test_REMOTE_8848.java
+++
@@ -1,9 +0,0 @@
-
-public class Test 
-{
-	public static void main(String[] args) {
-		System.out.println("master");
-		System.out.println("branch6781 commit");
-	}
-	
-}
